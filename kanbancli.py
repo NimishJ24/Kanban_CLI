334391b3623bc4eb71314651fe1d84905c7ab87d
NimishJ24 <jainnimish245@gmail.com>
2024-04-09 21:55:59 +0530

    setup

---
+++ kanbancli.py
@@ -0,0 +1,60 @@
+import click
+from click_default_group import DefaultGroup
+import yaml
+import os
+import sys
+from textwrap import wrap
+import collections
+import datetime
+import configparser
+import importlib
+
+class Config(object):
+    """The config in this example only holds aliases."""
+
+    def __init__(self):
+        self.path = os.getcwd()
+        self.aliases = {}
+
+    def read_config(self, filename):
+        parser = configparser.RawConfigParser()
+        parser.read([filename])
+        try:
+            self.aliases.update(parser.items('aliases'))
+        except configparser.NoSectionError:
+            pass
+
+pass_config = click.make_pass_decorator(Config, ensure=True) #ensure = True means object does not exist, it will be created and passed to the command.
+
+class AliasedGroup(DefaultGroup):
+    """This subclass of a group supports looking up aliases in a config
+    file and with a bit of magic.
+    """
+
+    def get_command(self, ctx, cmd_name):
+        # Step one: bulitin commands as normal
+        rv = click.Group.get_command(self, ctx, cmd_name)
+        if rv is not None:
+            return rv
+
+        # Step two: find the config object and ensure it's there.  This
+        # will create the config object is missing.
+        cfg = ctx.ensure_object(Config)
+
+        # Step three: lookup an explicit command aliase in the config
+        if cmd_name in cfg.aliases:
+            actual_cmd = cfg.aliases[cmd_name]
+            return click.Group.get_command(self, ctx, actual_cmd)
+
+        # Alternative option: if we did not find an explicit alias we
+        # allow automatic abbreviation of the command.  "status" for
+        # instance will match "st".  We only allow that however if
+        # there is only one command.
+        matches = [x for x in self.list_commands(ctx)
+                   if x.lower().startswith(cmd_name.lower())]
+        if not matches:
+            return None
+        elif len(matches) == 1:
+            return click.Group.get_command(self, ctx, matches[0])
+        ctx.fail('Too many matches: %s' % ', '.join(sorted(matches)))
+
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+import os
+
+# with open(os.path.join(os.getcwd(), 'VERSION')) as version_file:
+#     version = version_file.read().strip()
+
+# with open("README.md", "r") as fh:
+#     long_description = fh.read()
+
+setup(
+    name="kanbancli",
+    version='0.0.0',
+    description="Simple CLI-based Kanban board",
+    py_modules=['kanbancli'],
+    install_requires=[
+        'Click',
+        'click-default-group',
+        'pyyaml',
+        'rich'
+    ],
+    entry_points='''
+        [console_scripts]
+        kanbancli=kanbancli:kanbancli
+    ''',
+)
