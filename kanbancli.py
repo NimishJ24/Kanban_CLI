346541917b2a0bb3be3a56d835bd7a06f2769aa7
NimishJ24 <jainnimish245@gmail.com>
2024-04-12 17:49:00 +0530

    final

--- kanbancli.py
+++ kanbancli.py
@@ -1,4 +1,5 @@
 import click
+import urwid
 from click_default_group import DefaultGroup
 import yaml
 import os
@@ -16,22 +17,24 @@ class Config(object):
     """The config in this example only holds aliases."""
 
     def __init__(self):
-        self.path = os.getcwd()
+        self.path = os.getcwd() #get the current working directory
         self.aliases = {}
 
     def read_config(self, filename):
-        parser = configparser.RawConfigParser()
+        parser = configparser.RawConfigParser()#used to read config file in raw format 
         parser.read([filename])
         try:
             self.aliases.update(parser.items('aliases'))
         except configparser.NoSectionError:
             pass
 
-pass_config = click.make_pass_decorator(Config, ensure=True) #ensure = True means object does not exist, it will be created and passed to the command.
+pass_config = click.make_pass_decorator(Config, ensure=True)
+#make a decorator allowing click command to access files  
+#ensure = True means object does not exist, it will be created and passed to the command.
 
 class AliasedGroup(DefaultGroup):
     """This subclass of a group supports looking up aliases in a config
-    file and with a bit of magic.
+    file
     """
 
     def get_command(self, ctx, cmd_name):
@@ -49,10 +52,6 @@ class AliasedGroup(DefaultGroup):
             actual_cmd = cfg.aliases[cmd_name]
             return click.Group.get_command(self, ctx, actual_cmd)
 
-        # Alternative option: if we did not find an explicit alias we
-        # allow automatic abbreviation of the command.  "status" for
-        # instance will match "st".  We only allow that however if
-        # there is only one command.
         matches = [x for x in self.list_commands(ctx)
                    if x.lower().startswith(cmd_name.lower())]
         if not matches:
@@ -99,18 +98,14 @@ PRIORITY_MAP = {
     'low': 3
 }
 @kanbancli.command()
+@click.option('--name', '-n', required=True)
+@click.option('--priority', type=click.Choice(['high', 'medium', 'low']), default='medium')
 @click.argument('tasks',nargs=-1) # the number of command-line arguments that should be read
-@click.option('--name', '-n', help='Name of the Table', required=True)
-@click.option('--priority', type=click.Choice(['high', 'medium', 'low']), default='medium', help='Priority of the task' )
 def add(tasks,priority,name):
     """Add a tasks in todo"""
     config = read_config_yaml()
     dd = read_data(config)
-
-    if ('limits' in config and 'taskname' in config['limits']):
-        taskname_length = config['limits']['taskname']
-    else:
-        taskname_length = 40
+    taskname_length = 40
 
     for task in tasks:
         if len(task) > taskname_length:
@@ -132,14 +127,11 @@ def add(tasks,priority,name):
                            % (new_id, task))
 
     write_data(config, dd)
-    if ('repaint' in config and config['repaint']):
-        display()
-
 
 @kanbancli.command()
 @click.argument('ids', nargs=-1)
 def delete(ids):
-    """Delete task"""
+    """Delete task on the basis of task id"""
     config = read_config_yaml()
     dd = read_data(config)
 
@@ -158,13 +150,11 @@ def delete(ids):
             click.echo('Invalid task id')
 
     write_data(config, dd)
-    if ('repaint' in config and config['repaint']):
-        display()
 
 @kanbancli.command()
 @click.argument('ids', nargs=-1)
 def promote(ids):
-    """Promote task"""
+    """Promote task on the basis of task id"""
     config = read_config_yaml()
     dd = read_data(config)
     todos,plans, inprogs, dones = split_items(config, dd)
@@ -200,14 +190,11 @@ def promote(ids):
             click.echo('Invalid task id')
 
     write_data(config, dd)
-    if ('repaint' in config and config['repaint']):
-        display()
-
 
 @kanbancli.command()
 @click.argument('ids', nargs=-1)
 def regress(ids):
-    """Regress task"""
+    """Regress task on the basis of task id"""
     config = read_config_yaml()
     dd = read_data(config)
 
@@ -230,8 +217,6 @@ def regress(ids):
             click.echo('Already in todo, can not regress %s' % id)
 
     write_data(config, dd)
-    if ('repaint' in config and config['repaint']):
-        display()
 
 @kanbancli.command()
 @click.option('--name', '-n', help='Name of the Table', required=True)
@@ -246,11 +231,6 @@ def show(name):
     else:
         dones = dones[0:10]
 
-    # todos = '\n'.join([str(x) for x in todos])
-    # plans = '\n'.join([str(x) for x in plans])
-    # inprogs = '\n'.join([str(x) for x in inprogs])
-    # dones = '\n'.join([str(x) for x in dones])
-    # Filter data based on the name argument
     sorted_data = sorted(dd['data'].items(), key=lambda item: item[1][4])
     filtered_data = [item for item in sorted_data if item[1][5] == name]  # Assuming 6th element is at index 5
 
@@ -285,51 +265,82 @@ def show(name):
     table.add_column(
         '[bold magenta]done[/bold magenta]',
         no_wrap=True,
-        # footer="v.{}".format(VERSION)
+        footer="GDSC ROCKS"
     )
 
     table.add_row(todos,plans, inprogs, dones)
     console.print(table)
 
+class KanbanCLIWidget(urwid.WidgetWrap):
+    def __init__(self):
+        super().__init__(self.main_view())
+    
+
+    def main_view(self):
+        self.config = read_config_yaml()
+        self.dd = read_data(self.config)
+
+        tasks = self.get_task_list()
+        self.task_widgets = self.create_task_widgets(tasks)
+
+        # Create a ListBox to hold task widgets
+        self.list_box = urwid.ListBox(urwid.SimpleFocusListWalker(self.task_widgets))
+
+        return self.list_box
+
+    def get_task_list(self):
+        sorted_data = sorted(self.dd['data'].items(), key=lambda item: item[1][4])
+        return [("[%d] %s" % (key, value[1]), key) for key, value in sorted_data]
+    
+    def create_task_widgets(self, tasks):
+        return [urwid.Text(task[0]) for task in tasks]
+
+    
+
+    def mouse_event(self, size, event, button, col, row, focus):
+        if event == 'mouse press':
+            # Handle mouse press event
+            # Determine the task being clicked on and start drag operation
+            self.dragging_index = self.list_box.focus
+            if self.dragging_index is not None:
+                self.dragging = self.task_widgets[int(self.dragging_index)]
+        elif event == 'mouse release':
+            # Handle mouse release event
+            # Determine drop location and perform drop operation
+            if hasattr(self, 'dragging'):
+                target_index = self.list_box.focus_position[1]
+                self.drop_task(target_index)
+                delattr(self, 'dragging')
+                delattr(self, 'dragging_index')
+        elif event == 'mouse drag':
+            # Handle mouse drag event
+            # Update display to reflect drag operation
+            if hasattr(self, 'dragging'):
+                self.list_box.body[self.dragging_index] = urwid.Text('Dragging: ' + self.dragging.text)
+        return True
+    
+    def keypress(self, size, key):
+        # If the keypress is a mouse event, handle it in the mouse_event method
+        if urwid.is_mouse_event(key):
+            return self.mouse_event(size, *key)
+        else:
+            return super().keypress(size, key)
+
+    def drop_task(self, target_index):
+        if target_index != self.dragging_index:
+            # Reorder tasks based on drag and drop
+            task_id_to_move = self.task_widgets[self.dragging_index].original_widget.task_id
+            task_to_move = self.dd['data'].pop(task_id_to_move)
+            self.dd['data'].insert(target_index, task_to_move)
+            # Update task widgets
+            self.task_widgets = self.create_task_widgets(self.get_task_list())
+            # Update list box
+            self.list_box.body = urwid.SimpleFocusListWalker(self.task_widgets)
+            # Write updated data to the file
+            write_data(self.config, self.dd)
 
 
 #FUNCTIONS USED ABOVE :
-
-
-def display():
-    console = Console()
-    """Show tasks in kanbancli"""
-    config = read_config_yaml()
-    dd = read_data(config)
-    todos,plans, inprogs, dones = split_items(config, dd)
-    if 'limits' in config and 'done' in config['limits']:
-        dones = dones[0:int(config['limits']['done'])]
-    else:
-        dones = dones[0:10]
-
-    todos = '\n'.join([str(x) for x in todos])
-    plans = '\n'.join([str(x) for x in plans])
-    inprogs = '\n'.join([str(x) for x in inprogs])
-    dones = '\n'.join([str(x) for x in dones])
-
-    table = Table(show_header=True, show_footer=True)
-    table.add_column(
-        "[bold yellow]todo[/bold yellow]",
-        no_wrap=True,
-        footer="GDSC ROCKS"
-    )
-    table.add_column('[bold blue]plan[/bold blue]', no_wrap=True)
-    table.add_column('[bold green]in-progress[/bold green]', no_wrap=True)
-    table.add_column(
-        '[bold magenta]done[/bold magenta]',
-        no_wrap=True,
-        # footer="v.{}".format(VERSION)
-    )
-
-    table.add_row(todos,plans, inprogs, dones)
-    console.print(table)
-
-
 
 def read_data(config):
     """Read the existing data from the config datasource"""
@@ -347,12 +358,10 @@ def read_data(config):
         with open(config["kanbancli_data"], 'r') as stream:
             return yaml.safe_load(stream)
 
-
 def write_data(config, data):
     """Write the data to the config datasource"""
     with open(config["kanbancli_data"], 'w') as outfile:
         yaml.dump(data, outfile, default_flow_style=False)
-
 
 def split_items(config, dd):
     todos = []
@@ -374,14 +383,13 @@ def split_items(config, dd):
 
     return todos,plans, inprogs, dones
 
-
 def read_config_yaml():
     """Read the app config from ~/.kanbancli.yaml"""
     try:
         home = get_kanbancli_home()
-        with open(home + "/.kanbancli.yaml", 'r') as stream:
+        with open(home + "/.kanbancli.yaml", 'r') as ymfl:
             try:
-                return yaml.safe_load(stream)
+                return yaml.safe_load(ymfl)
             except yaml.YAMLError:
                 print("Ensure %s/.kanbancli.yaml is valid, expected YAML." % home)
                 sys.exit()
@@ -389,16 +397,26 @@ def read_config_yaml():
         print("Ensure %s/.kanbancli.yaml exists and is valid." % home)
         sys.exit()
 
-
-
 def get_kanbancli_home():
-    home = os.environ.get('KANBANCLI_HOME')
-    if not home:
-        home = os.path.expanduser('~')
+    home = os.path.expanduser('~')
     return home
 
 def timestamp():
     return '{:%Y-%b-%d %H:%M:%S}'.format(datetime.datetime.now())
+
+#   MOUSE CONFIG
+def main():
+    # Create KanbanCLIWidget instance
+    kanban_cli_widget = KanbanCLIWidget()
+
+    # Create a loop widget to handle input and draw the UI
+    loop = urwid.MainLoop(kanban_cli_widget)
+
+    # Run the UI loop
+    loop.run()
+
+if __name__ == "__main__":
+    main()
 
 
 
